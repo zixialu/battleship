9ae68a579a0fd6750f2b2d050b2adc9be331f320
Zixia Lu <zixialu@gmail.com>
2018-12-02 19:42:14 +0000

    Added method to initialize a game

--- battleship.js
+++ battleship.js
@@ -1,1 +1,18 @@
 const board = require('./scripts/board');
+const history = require('./scripts/history');
+
+
+var playerOrder = [];
+
+const startGame = function setupGameObjects() {
+  // Instantiate Players
+  // TODO: Implement logic for who goes first, and player naming
+  const p1 = "Player 1";
+  const p2 = "Player 2";
+  playerOrder = [p1, p2];
+  board.newPlayer(p1);
+  board.newPlayer(p2);
+
+  // Initialize History
+  history.initializeHistory(board.getBoards(), playerOrder);
+};
