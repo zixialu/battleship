b00cb1cbe4c494daba95dc40a0e236bce4289ca4
Zixia Lu <zixialu@gmail.com>
2018-12-07 16:22:13 -0500

    Set up express and ejs

--- battleship.js
+++ battleship.js
@@ -1,12 +1,19 @@
+const express = require('express');
 const board = require('./scripts/board');
 const history = require('./scripts/history');
 
+const app = express();
+const PORT = 8080;
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
 
 var playerOrder = [];
 var gameOver = false;
 
-const gameLoop = function () {
-  while(!gameOver) {
+// MARK: - Game Loop
+const gameLoop = function() {
+  while (!gameOver) {
     for (player of playerOrder) {
       if (!getBoards[player].isAlive) {
         // TODO: Implement game over
@@ -22,8 +29,8 @@ const gameLoop = function () {
 const startGame = function setupGameObjects() {
   // Instantiate Players
   // TODO: Implement logic for who goes first, and player naming
-  const p1 = "Player 1";
-  const p2 = "Player 2";
+  const p1 = 'Player 1';
+  const p2 = 'Player 2';
   playerOrder = [p1, p2];
   board.newPlayer(p1);
   board.newPlayer(p2);
@@ -31,9 +38,20 @@ const startGame = function setupGameObjects() {
   // Initialize History
   history.initializeHistory(board.getBoards(), playerOrder);
 
-  // Start Game
+  // Start game loop
   gameLoop();
 };
 
+// MARK: - Endpoints
+app.get('/', (req, res) => {
+  res.render('game');
+});
 
+//MARK: - Ports
+// Listening for requests
+app.listen(PORT, () => {
+  console.log(`tinyApp listening on port ${PORT}!`);
+});
+
+// MARK: - Start
 startGame();
