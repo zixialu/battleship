2fb0421243d0dcf7dc8aad0ce4656b4558ff3b23
Zixia Lu <zixialu@gmail.com>
2018-12-08 17:18:12 -0500

    Implement leaderboard methods

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,5 +1,19 @@
 const model = require('/models/leaderboard');
+
 const get = function getLeaderboard() {
   // TODO: Make and return a deep copy instead of the original
   return model;
 };
+
+const post = function postScoreToLeaderboard(name, score) {
+  // Add new score to table
+  model.push({
+    name,
+    date: new Date(),
+    score
+  });
+  // Sort table
+  model.sort((a, b) => a.score - b.score);
+};
+
+module.exports = { get, post };
